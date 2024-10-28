c6a58ebb46824154c3dae58dcf9300ae1e6b5f19
김두열 <enduf768640@gmail.com>
2024-10-28 15:02:42 +0900

    [FIX] #3 OAuth 로그인 API UseCase 호출 누락 수정

--- Backend/user-service/src/main/java/com/ddasoom/user_service/user/adapter/in/web/LoginController.java
+++ Backend/user-service/src/main/java/com/ddasoom/user_service/user/adapter/in/web/LoginController.java
@@ -1,7 +1,13 @@
 package com.ddasoom.user_service.user.adapter.in.web;
 
+import static com.ddasoom.user_service.common.util.ApiUtils.success;
+
 import com.ddasoom.user_service.common.annotation.WebAdapter;
+import com.ddasoom.user_service.common.util.ApiUtils.ApiResult;
 import com.ddasoom.user_service.user.adapter.in.web.request.LoginRequest;
+import com.ddasoom.user_service.user.adapter.in.web.response.LoginResponse;
+import com.ddasoom.user_service.user.application.port.in.LoginCommand;
+import com.ddasoom.user_service.user.application.port.in.LoginUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LoginController {
 
-    @PostMapping("/api/users/login")
-    public void login(@RequestBody LoginRequest request) {
+    private final LoginUseCase loginUseCase;
 
+    @PostMapping("/api/users/login")
+    public ApiResult<LoginResponse> login(@RequestBody LoginRequest request) {
+        LoginCommand command = new LoginCommand(
+                request.email(),
+                request.name()
+        );
+
+        return success(loginUseCase.login(command));
     }
 }
