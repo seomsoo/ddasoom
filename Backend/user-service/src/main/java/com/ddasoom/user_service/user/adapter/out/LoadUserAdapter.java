51fed08a848eef4ec1f88538f965035e6ce0d4d0
김두열 <enduf768640@gmail.com>
2024-10-28 14:16:55 +0900

    [FEAT] #3 OAuth 로그인 API User DB 어댑터 구현

---
+++ Backend/user-service/src/main/java/com/ddasoom/user_service/user/adapter/out/LoadUserAdapter.java
@@ -0,0 +1,27 @@
+package com.ddasoom.user_service.user.adapter.out;
+
+import com.ddasoom.user_service.common.annotation.PersistenceAdapter;
+import com.ddasoom.user_service.user.application.domain.User;
+import com.ddasoom.user_service.user.application.port.out.LoadUserPort;
+import com.ddasoom.user_service.user.error.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class LoadUserAdapter implements LoadUserPort {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUser(String email) {
+        UserJpaEntity user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        return new User(
+                user.getId(),
+                user.getEmail(),
+                user.getName()
+        );
+    }
+}
