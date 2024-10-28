2c06d48aa75596c6a96d6be71eda185c9cf87b70
minyeong981 <rlaalsdud41@naver.com>
2024-10-28 14:46:26 +0900

    [REFACTOR] #11 파일 구조 리팩토링

--- Frontend/src/app/calendar/page.tsx
+++ Frontend/src/app/calendar/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Calendar from '@/components/Calendar/Calendar';
+import Navbar from '@/components/Navbar';
 export default function calendar() {
   return (
     <div>
-      <Calendar/>      
+      <Calendar />
       {/* 공황 일지 */}
       {/* 생활 기록 */}
+      <Navbar />
     </div>
   );
 }
--- Frontend/src/app/training/page.tsx
+++ Frontend/src/app/training/page.tsx
@@ -1,41 +1,59 @@
+import Image from 'next/image';
 import React from 'react';
 
+import DdasomiSvg from '@/asset/Svg/ddasomi.svg';
+import SunglassDasomiSvg from '@/asset/Svg/sunglassDdasomi.svg';
 import Navbar from '@/components/Navbar';
+import Link from 'next/link';
 
 export default function Training() {
   return (
-    <div className="flex flex-col items-center gap-6 mt-14 max-h-screen  ">
-      <div className="flex ml-8 bg-main4 border border-r-0 border-main1 shadow-md rounded-l-2xl w-full max-w-md p-4 py-6 mb-4 ">
-        <div className="flex flex-col gap-2">
-          <h2 className="text-3xl font-nanumBold">호흡 연습</h2>
-          <p className="text-sm">
-            세 가지 호흡법 중 <br />
-            나에게 맞는 방법을 골라 연습해보세요.
-          </p>
-        </div>
+    <div className="flex flex-col items-center mt-14 max-h-screen relative">
+      <div className="relative w-full flex ml-6">
+        <Image src={DdasomiSvg} alt="DdasomiSvg" className="absolute -top-14 w-32 h-32 z-0 " />
       </div>
-
-      <div className="flex mr-8 items-start border-l-0 bg-main4 border border-main1 shadow-md rounded-r-2xl w-full max-w-md p-4 py-6 mb-4">
-        <div className="flex flex-col gap-2 text-right flex-grow">
-          <h2 className="text-3xl font-nanumBold">그라운딩</h2>
-          <div className="text-sm">
-            <p className="mr-1">감각에 집중하여</p>
-            <p> 지금 이 순간을 온전히 느껴보세요.</p>
+      <div className="flex ml-8 mt-7 bg-main4 border border-r-0 border-main1 shadow-md rounded-l-2xl w-full max-w-md p-4 py-6 mb-4 z-10">
+        <Link href="/training/breath">
+          <div className="flex flex-col gap-2 ml-2">
+            <h2 className="text-3xl font-nanumBold">호흡 연습</h2>
+            <p className="text-sm">
+              세 가지 호흡법 중 <br />
+              나에게 맞는 방법을 골라 연습해보세요.
+            </p>
           </div>
-        </div>
+        </Link>
       </div>
 
-      <div className=" flex ml-8 border-r-0 bg-main4 border border-main1 shadow-md rounded-l-2xl w-full max-w-md p-4 py-6 mb-16">
-        <div className="flex flex-col gap-2">
-          <h2 className="text-3xl font-nanumBold">안정화 기법</h2>
-          <div className="text-sm">
-            <p className="ml-[0.8px]">자연의 움직임을 보며</p>
-            <p>긴장된 마음과 몸을 풀어보세요.</p>
+      <div className="flex mr-8 items-start mt-7 border-l-0 bg-main4 border border-main1 shadow-md rounded-r-2xl w-full max-w-md p-4 py-6 mb-4 z-10">
+        <Link href="/training/grounding">
+          <div className="flex flex-col gap-2 text-right ml-20 relative">
+            <h2 className="text-3xl font-nanumBold">그라운딩</h2>
+            <div className="absolute -top-6 -left-[120px]  transform rotate-12">
+              <Image src={DdasomiSvg} alt="DdasomiSvg" className="w-32 h-32 z-2" />
+            </div>
+            <div className="text-sm">
+              <p className="mr-1">감각에 집중하여</p>
+              <p> 지금 이 순간을 온전히 느껴보세요.</p>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
 
-      {/* Navbar */}
+      <div className="relative w-full flex z-0">
+        <Image src={SunglassDasomiSvg} alt="DdasomiSvg" className="absolute w-32 h-32 -top-8 right-2 z-0" />
+      </div>
+      <div className="flex ml-8 mt-7 bg-main4 border border-r-0 border-main1 shadow-md rounded-l-2xl w-full max-w-md p-4 py-6 mb-4 z-10">
+        <Link href="/training/calmDown">
+          <div className="flex flex-col gap-2 ml-2">
+            <h2 className="text-3xl font-nanumBold">안정화 기법</h2>
+            <p className="text-sm">
+              자연의 움직임을 보며 <br />
+              긴장된 마음과 몸을 풀어보세요.
+            </p>
+          </div>
+        </Link>
+      </div>
+
       <Navbar />
     </div>
   );
