dfa849d8e9d5e8c6676eb2d97969db4b6948cf29
김두열 <enduf768640@gmail.com>
2024-10-29 15:36:48 +0900

    [FEAT] #22 Command 객체 Validation 로직 추가

--- Backend/user-service/src/main/java/com/ddasoom/user_service/user/application/port/in/JoinCommand.java
+++ Backend/user-service/src/main/java/com/ddasoom/user_service/user/application/port/in/JoinCommand.java
@@ -1,8 +1,19 @@
 package com.ddasoom.user_service.user.application.port.in;
 
+import static com.ddasoom.user_service.common.util.ValidationUtils.validate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
 public record JoinCommand(
-        String email,
-        String name
+        @NotEmpty @Email String email,
+        @NotEmpty String name
 ) {
 
+    public JoinCommand(String email, String name) {
+        this.email = email;
+        this.name = name;
+
+        validate(this);
+    }
 }
