84b9abfb0a0136f4a7e385f8b64941060a32c898
김두열 <enduf768640@gmail.com>
2024-10-30 17:44:29 +0900

    [FEAT] #33 유저 별 AI 목소리 목록 조회 DB 어댑터 구현

--- Backend/voice-service/src/main/java/com/ddasoom/voice_service/voice/adapter/out/VoiceRepository.java
+++ Backend/voice-service/src/main/java/com/ddasoom/voice_service/voice/adapter/out/VoiceRepository.java
@@ -1,7 +1,9 @@
 package com.ddasoom.voice_service.voice.adapter.out;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoiceRepository extends JpaRepository<VoiceJpaEntity, Long> {
 
+    List<VoiceJpaEntity> findByUserId(Long userId);
 }
