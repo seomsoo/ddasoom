92b5a043a36df1db989e100eb6ad2a40c7c42823
Seo Minsu <minsu9812@naver.com>
2024-11-01 01:31:50 +0900

    [FEAT] #35 안정화 기법 영상추가 및 퍼블리싱

--- Frontend/src/components/Training/VideoData.ts
+++ Frontend/src/components/Training/VideoData.ts
@@ -4,23 +4,27 @@ import windVideo from '@/asset/Video/wind.mp4';
 
 type VideoData = {
   src?: string;
-  text: string;
+  introText: string;
+  focusText: string;
   type: 'video' | 'text';
 };
 
 export const videoData: Record<string, VideoData> = {
   fireplace: {
-    src: fireplaceVideo, // import한 비디오 파일 사용
-    text: '상단의 소리를 키우고 캠프파이어에 집중하세요',
+    src: fireplaceVideo,
+    introText: '상단의 소리를 키우고',
+    focusText: '따뜻함을 느껴보세요.',
     type: 'video',
   },
   wind: {
-    src: windVideo, // import한 비디오 파일 사용
-    text: '상단의 소리를 키우고 바람을 느껴보세요',
+    src: windVideo,
+    introText: '상단의 소리를 키우고',
+    focusText: '바람을 느껴보세요.',
     type: 'video',
   },
   other: {
-    text: '이 콘텐츠는 비디오가 아닌 다른 유형입니다.',
+    introText: '이 콘텐츠는 비디오가 아닌',
+    focusText: '다른 유형입니다.',
     type: 'text',
   },
 };
