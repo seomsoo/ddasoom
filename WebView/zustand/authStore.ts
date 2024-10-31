87902a03db4824db4fb694d99a152dc691e17016
monam2 <kangcw0107@gmail.com>
2024-10-31 14:09:35 +0900

    [FEAT] 로그인/회원가입 관련 스토어 생성

---
+++ WebView/zustand/authStore.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      token: "",
+      userEmail: "",
+      userName: "",
+      userId: 0,
+      setToken: (token: Token) => set({ token: token }),
+      setUserEmail: (email: Email) => set({ userEmail: email }),
+      setUserName: (name: Name) => set({ userName: name }),
+      setUserId: (id: UserId) => set({ userId: id }),
+      clearToken: () => set({ token: "" }),
+    }),
+    {
+      name: "auth-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+      partialize: (state) => ({
+        token: state.token,
+      }),
+    },
+  ),
+);
+
+export default useAuthStore;
