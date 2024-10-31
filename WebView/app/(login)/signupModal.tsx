cd28d300ac8eae61c8d8a7aabc59706c82f91f45
monam2 <kangcw0107@gmail.com>
2024-10-31 14:10:37 +0900

    [FEAT] #36 로그인 에러 시 회원가입 페이지로 이동

---
+++ WebView/app/(login)/signupModal.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SignupModal = () => {
+  return (
+    <View>
+      <Text>signupModal</Text>
+    </View>
+  );
+};
+
+export default SignupModal;
