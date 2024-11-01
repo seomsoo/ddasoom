ea8a14a319ab33842fe3bf354d281d44a2b2cee6
minyeong981 <rlaalsdud41@naver.com>
2024-11-01 10:24:56 +0900

    [REFATOR] #46 자가진단 페이지 리팩토링

--- Frontend/src/app/record/selfDiagnosis/page.tsx
+++ Frontend/src/app/record/selfDiagnosis/page.tsx
@@ -3,26 +3,26 @@ import Link from 'next/link';
 import Ddasomi from '@/asset/Svg/shadowDdasomi.svg';
 import Header from '@/components/Header';
 
-
 export default function SelfDiagnosisPage() {
   return (
     <div>
       <Header label="자가진단" />
-      <div className='flex flex-col'>
-        <div className="flex flex-col justify-center items-center gap-5 mt-14">
-          <p className="font-hakgyoansimR text-2xl text-center">
-        보건복지부 국립건강센터 <br />
-        공황장애 진단 기준에 따른 <br /> 자가진단입니다.
-          </p>
+      <main className="flex flex-col">
+        <section className="flex flex-col justify-center items-center gap-5 mt-14">
+          <h3 className="font-hakgyoansimR text-2xl text-center">
+            보건복지부 국립건강센터 <br />
+            공황장애 진단 기준에 따른 <br /> 자가진단입니다.
+          </h3>
           <p className="text-xs">총 13문항으로, 다음 중 나타난 증상을 모두 골라주세요.</p>
-          <Ddasomi className="mt-4" />  
-        </div>
+          <Ddasomi className="mt-4" />
+        </section>
 
-        <Link href='/record/selfDiagnosis/check' className="font-nanumBold text-xl w-full py-3 mt-10 rounded-3xl text-main3 text-center bg-button1">
-      시작하기
+        <Link
+          href="/record/selfDiagnosis/check"
+          className="font-nanumBold text-xl w-full py-3 mt-10 rounded-3xl text-main3 text-center bg-button1">
+          시작하기
         </Link>
-      </div>
+      </main>
     </div>
-
   );
 }
