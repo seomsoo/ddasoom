ffc829682c26e7bdc95f9082d766bca5aab00fd8
monam2 <kangcw0107@gmail.com>
2024-11-01 11:16:27 +0900

    [FEAT] #39 폰트 적용 코드 수정

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -7,33 +7,33 @@ import ReactQueryProvider from '@/utils/reactQueryProvider';
 import ReduxProvider from '@/utils/reduxProvider';
 
 const nanumRegular = localFont({
-  src: '../fonts/NanumSquareNeoOTF-Rg.otf',
+  src: '../../public/fonts/NanumSquareNeoOTF-Rg.otf',
   variable: '--font-nanumRegular',
 });
 const nanumLight = localFont({
-  src: '../fonts/NanumSquareNeoOTF-Lt.otf',
+  src: '../../public/fonts/NanumSquareNeoOTF-Lt.otf',
   variable: '--font-nanumLight',
 });
 const nanumBold = localFont({
-  src: '../fonts/NanumSquareNeoOTF-Bd.otf',
+  src: '../../public/fonts/NanumSquareNeoOTF-Bd.otf',
   variable: '--font-nanumBold',
 });
 const nanumExtraBold = localFont({
-  src: '../fonts/NanumSquareNeoOTF-Eb.otf',
+  src: '../../public/fonts/NanumSquareNeoOTF-Eb.otf',
   variable: '--font-nanumExtraBold',
 });
 const nanumHeavy = localFont({
-  src: '../fonts/NanumSquareNeoOTF-Hv.otf',
+  src: '../../public/fonts/NanumSquareNeoOTF-Hv.otf',
   variable: '--font-nanumHeavy',
 });
 
 const hakgyoansimR = localFont({
-  src: '../fonts/Hakgyoansim-Dunggeunmiso-OTF-R.otf',
+  src: '../../public/fonts/Hakgyoansim-Dunggeunmiso-OTF-R.otf',
   variable: '--font-hakgyoansimR',
 });
 
 const hakgyoansimB = localFont({
-  src: '../fonts/Hakgyoansim-Dunggeunmiso-OTF-B.otf',
+  src: '../../public/fonts/Hakgyoansim-Dunggeunmiso-OTF-B.otf',
   variable: '--font-hakgyoansimB',
 });
 
