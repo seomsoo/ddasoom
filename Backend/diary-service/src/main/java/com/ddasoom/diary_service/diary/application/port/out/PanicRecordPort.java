bb4e0fa6396da8dcc09bfc4842f2382d24bb7aea
ShinBeomKim <shinbeomkim11@gmail.com>
2024-11-01 17:18:04 +0900

    [Feat] #54 DIARY-SERVICE에서 가장 최근에 발생한 공황발작한 기록 반환하는 기능 추가

--- Backend/diary-service/src/main/java/com/ddasoom/diary_service/diary/application/port/out/PanicRecordPort.java
+++ Backend/diary-service/src/main/java/com/ddasoom/diary_service/diary/application/port/out/PanicRecordPort.java
@@ -1,6 +1,7 @@
 package com.ddasoom.diary_service.diary.application.port.out;
 
 import com.ddasoom.diary_service.diary.application.domain.PanicRecordInfo;
+import com.ddasoom.diary_service.diary.application.domain.PanicSimple;
 import com.ddasoom.diary_service.diary.application.port.in.PanicDescriptionCommand;
 
 public interface PanicRecordPort {
@@ -8,4 +9,5 @@ public interface PanicRecordPort {
     PanicRecordInfo getPanicRecord(Long userId, int year, int month, int day);
     void savePanicRecord(PanicRecordInfo panic);
     void savePanicDescription(PanicDescriptionCommand command);
+    PanicSimple getPanicSimple(Long userId);
 }
