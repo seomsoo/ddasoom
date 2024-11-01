aee35dea27eb9859cb9a7bf88216c5359fdc4a8f
monam2 <kangcw0107@gmail.com>
2024-11-01 17:02:24 +0900

    [FEAT] #52 axios 인스턴스 사용 및 api 함수 변경

--- WebView/services/auth.ts
+++ WebView/services/auth.ts
@@ -1,16 +1,13 @@
-import axios from "axios";
+import { instance } from "@/config/axios";
 
 /** 회원가입 */
 export const signUp = async (signupForm: SignupForm) => {
-  await axios.post<BaseResponse<void>>("/api/users", signupForm);
+  await instance.post<BaseResponse<void>>("/api/users", signupForm);
 };
 
 /** 로그인 */
 export const signIn = async (email: Email) => {
-  const { data } = await axios.post<BaseResponse<SignInResponseDto>>(
-    "/api/users/login",
-    { email },
-  );
+  const { data } = await instance.post<BaseResponse<SignInResponseDto>>("/api/users/login", { email });
 
   return data.data;
 };
