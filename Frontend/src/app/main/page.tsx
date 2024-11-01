1570b305d46660039201625409cb69a8fe3dbada
monam2 <kangcw0107@gmail.com>
2024-11-01 17:00:19 +0900

    [FEAT] #52 웹페이지-웹뷰 통신 테스트용 버튼 추가

--- Frontend/src/app/main/page.tsx
+++ Frontend/src/app/main/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import TempComp from '@/components/TempComp';
+
 const Main = () => {
-  return <div>main</div>;
+  return (
+    <>
+      <div>main</div>
+      <TempComp />
+    </>
+  );
 };
 
 export default Main;
