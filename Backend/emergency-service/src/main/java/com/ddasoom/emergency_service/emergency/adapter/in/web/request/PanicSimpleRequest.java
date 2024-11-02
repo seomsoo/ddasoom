82d0595ab17958d76a5544ee7d2d46a461552f1d
ShinBeomKim <shinbeomkim11@gmail.com>
2024-11-02 19:28:43 +0900

    [FEAT] #54 30분 전 발생한 공황 발작 조회 기능 추가

---
+++ Backend/emergency-service/src/main/java/com/ddasoom/emergency_service/emergency/adapter/in/web/request/PanicSimpleRequest.java
@@ -0,0 +1,8 @@
+package com.ddasoom.emergency_service.emergency.adapter.in.web.request;
+
+import com.ddasoom.emergency_service.emergency.application.domain.PanicSimple;
+
+public record PanicSimpleRequest(
+        PanicSimple data
+) {
+}
