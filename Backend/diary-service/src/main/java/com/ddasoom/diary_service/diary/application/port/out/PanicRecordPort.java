90e62c3fea965dfd7f7b903deabb83efa7c66b7f
박지훈 <pjh2996@naver.com>
2024-11-04 00:22:56 +0900

    [FIX] #51 캘린더 상세 조회 시 하루에 공황이 여러번 발생 한 경우 반환값 추가

--- Backend/diary-service/src/main/java/com/ddasoom/diary_service/diary/application/port/out/PanicRecordPort.java
+++ Backend/diary-service/src/main/java/com/ddasoom/diary_service/diary/application/port/out/PanicRecordPort.java
@@ -2,10 +2,13 @@ package com.ddasoom.diary_service.diary.application.port.out;
 
 import com.ddasoom.diary_service.diary.application.domain.PanicRecordInfo;
 import com.ddasoom.diary_service.diary.application.port.in.PanicDescriptionCommand;
+import java.util.List;
 
 public interface PanicRecordPort {
 
-    PanicRecordInfo getPanicRecord(Long userId, int year, int month, int day);
+    List<PanicRecordInfo> getPanicRecord(Long userId, int year, int month, int day);
+
     void savePanicRecord(PanicRecordInfo panic);
+
     void savePanicDescription(PanicDescriptionCommand command);
 }
