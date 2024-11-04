6f0d74e47d61cc9d6e074d9391cfb3a99d42f9a9
minyeong981 <rlaalsdud41@naver.com>
2024-11-04 14:08:04 +0900

    [FEAT] #47 훈련 결과 페이지 랜덤 문구 생성

--- Frontend/src/components/Record/SelfDiagnosis/CheckList.tsx
+++ Frontend/src/components/Record/SelfDiagnosis/CheckList.tsx
@@ -6,34 +6,13 @@ import React, { useState } from 'react';
 import Button from '@/components/Button';
 import ProgressBar from '@/components/Record/SelfDiagnosis/ProgressBar';
 import QuestionText from '@/components/Record/SelfDiagnosis/QuestionText';
+import { questions } from '@/constants/SelfDiagnosisData';
 
-
-const questions = [
-  { id: '1', text: '심장이 두근거리고, 맥박이 빨라진다.', options: ['네', '아니요'] },
-  { id: '2', text: '심하게 땀을 흘린다.', options: ['네', '아니요'] },
-  { id: '3', text: '두려움으로 인한, 손발 몸에 떨림이 느껴진다.', options: ['네', '아니요'] },
-  { id: '4', text: '숨이 가쁘고, 숨 막히는 느낌이 든다.', options: ['네', '아니요'] },
-  { id: '5', text: '질식할 것 같은 기분이 든다.', options: ['네', '아니요'] },
-  { id: '6', text: '가슴이 답답하고, 불편감이 느껴진다.', options: ['네', '아니요'] },
-  { id: '7', text: '구토감이 느껴지고, 속이 불편해진다.', options: ['네', '아니요'] },
-  { id: '8', text: '현기증이 오고, 어지럽거나 쓰러질 것 같다.', options: ['네', '아니요'] },
-  {
-    id: '9',
-    text: '내가 아닌 것 같고, 비현실적인 느낌이 든다.',
-    options: ['네', '아니요'],
-  },
-  { id: '10', text: '혹시 이러다 미치는 게, 아닐까 하는 두려움에, 사로잡히게 된다.', options: ['네', '아니요'] },
-  { id: '11', text: '죽을 것 같은 공포감이 든다.', options: ['네', '아니요'] },
-  { id: '12', text: '감각이 둔해지거나, 따끔거리는 느낌이 든다.', options: ['네', '아니요'] },
-  { id: '13', text: '몸에 오한이 나거나, 얼굴이 화끈거린다.', options: ['네', '아니요'] },
-];
-  
 export default function CheckList() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [yesCount, setYesCount] = useState(0);
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const router = useRouter();
-  
 
   const handleOptionClick = (option: string) => {
     setSelectedOption(option);
@@ -57,7 +36,7 @@ export default function CheckList() {
   const progress = ((currentQuestionIndex + 1) / questions.length) * 100;
 
   return (
-    <div>      
+    <div>
       <div className=" flex flex-col items-center ">
         <ProgressBar progress={progress} />
         <QuestionText text={currentQuestion.text} />
@@ -76,10 +55,9 @@ export default function CheckList() {
         </div>
       </div>
 
-
       <div className="mt-36 w-full ">
         <Button onClick={handleNextClick} label="다음" disabled={!selectedOption} />
-      </div>  
+      </div>
     </div>
   );
 }
