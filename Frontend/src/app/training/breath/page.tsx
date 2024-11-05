2456cdd678a73ccfc2a59af5072b0af85ad2f4f0
서민수 <minsu9812@naver.com>
2024-11-05 16:55:02 +0900

    안정화기법 물멍 부분 추가

--- Frontend/src/app/training/breath/page.tsx
+++ Frontend/src/app/training/breath/page.tsx
@@ -1,25 +1,25 @@
-// import background from '@/components/BackGround/Background.module.css';
+import background from '@/components/BackGround/Background.module.css';
 import Header from '@/components/Header';
 import BreathSelector from '@/components/Training/Breath/BreathSelector';
-// import Hill from '@/svgs/hill.svg';
+import Backcloud from '@/svgs/backcloud.svg';
 export default function BreathTrainingPage() {
   return (
-    <section className={' absolute inset-0  flex justify-center overflow-hidden'}>
+    <section className={`${background.background4} absolute inset-0  flex justify-center overflow-hidden text-white`}>
       <div className="absolute top-9 left-2 w-full">
         <Header label="" />
       </div>
       <div className="absolute bottom-0 w-full">
-        {/* <Hill /> */}
+        <Backcloud />
       </div>
-      <div className="text-center">
+      <main>
         <h1 className="text-4xl font-hakgyoansimR mt-24 mb-5">
-          1분 동안 <br />
+          따솜이와 함께 <br />
           호흡에 집중해볼까요?
         </h1>
         <div className="flex items-center justify-center ">
           <BreathSelector />
         </div>
-      </div>
+      </main>
     </section>
   );
 }
