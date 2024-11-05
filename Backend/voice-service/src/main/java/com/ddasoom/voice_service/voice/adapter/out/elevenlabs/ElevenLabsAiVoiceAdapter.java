26e628ffe7b06dcacf5d196ed4bbc51e7c98ca0a
김두열 <enduf768640@gmail.com>
2024-11-05 09:41:56 +0900

    [REFACTOR] #70 ElevenLabs 어댑터 리팩토링

--- Backend/voice-service/src/main/java/com/ddasoom/voice_service/voice/adapter/out/elevenlabs/ElevenLabsAiVoiceAdapter.java
+++ Backend/voice-service/src/main/java/com/ddasoom/voice_service/voice/adapter/out/elevenlabs/ElevenLabsAiVoiceAdapter.java
@@ -1,108 +1,64 @@
 package com.ddasoom.voice_service.voice.adapter.out.elevenlabs;
 
+import static com.ddasoom.voice_service.voice.adapter.out.elevenlabs.ElevenLabsRequestUtils.sendRequest;
+import static com.ddasoom.voice_service.voice.adapter.out.elevenlabs.SpeechScript.speechScripts;
+
 import com.ddasoom.voice_service.voice.adapter.out.elevenlabs.request.TextToSpeechRequest;
+import com.ddasoom.voice_service.voice.adapter.out.elevenlabs.request.TrainAiVoiceRequest;
+import com.ddasoom.voice_service.voice.adapter.out.elevenlabs.response.TrainAiVoiceResponse;
 import com.ddasoom.voice_service.voice.application.domain.SoundFile;
 import com.ddasoom.voice_service.voice.application.domain.Voice;
 import com.ddasoom.voice_service.voice.application.port.out.ConvertTextScriptToSoundPort;
 import com.ddasoom.voice_service.voice.application.port.out.TrainAiVoicePort;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @RequiredArgsConstructor
 public class ElevenLabsAiVoiceAdapter implements TrainAiVoicePort, ConvertTextScriptToSoundPort {
 
-    private final ObjectMapper objectMapper;
-    private final RestTemplate restTemplate;
-
-    private final RestClient restClient;
-
     @Override
     public String trainAiVoice(List<Voice> voices) {
-        // API URL
-        String url = "https://api.elevenlabs.io/v1/voices/add";
+        List<ByteArrayResource> voiceFiles = voices.stream()
+                .map(this::convertByteArrayToResource)
+                .toList();
 
-        // 데이터 설정
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("name", UUID.randomUUID().toString());
+        TrainAiVoiceResponse response = sendRequest(
+                new TrainAiVoiceRequest(UUID.randomUUID().toString(), voiceFiles)
+        );
 
-        for (Voice voice : voices) {
-            ByteArrayResource fileResource = new ByteArrayResource(voice.bytes()) {
-                @Override
-                public String getFilename() {
-                    return "uploaded-file-" + System.currentTimeMillis(); // 파일 이름 지정
-                }
-            };
-            body.add("files", fileResource);
-        }
-
-        String response = restClient.post()
-                .uri(url)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
-                .header("xi-api-key", "sk_2ff2c593c65116b4f22f94c7ae2a77b77986f005c314683e")
-                .body(body)
-                .retrieve()
-                .body(String.class);
-
-        try {
-            return objectMapper.readTree(response)
-                    .get("voice_id")
-                    .asText();
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return response.voiceId();
     }
 
     @Override
     public List<SoundFile> convertTextScriptToSoundPort(String voiceKey) {
-        List<Script> scripts = SpeechScript.speechScripts();
-
-        return scripts.stream()
-                .map(script -> convert(voiceKey, script.code(), script.message()))
+        return speechScripts().stream()
+                .map(script -> getSoundFile(voiceKey, script))
                 .toList();
     }
 
-    private SoundFile convert(String voiceKey, String code, String script) {
-        String url = "https://api.elevenlabs.io/v1/text-to-speech/" + voiceKey;
+    private ByteArrayResource convertByteArrayToResource(Voice voice) {
+        return new ByteArrayResource(voice.bytes()) {
 
-        // 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("xi-api-key", "sk_2ff2c593c65116b4f22f94c7ae2a77b77986f005c314683e");
+            @Override
+            public String getFilename() {
+                return UUID.randomUUID().toString();
+            }
+        };
+    }
 
-        // JSON 본문 설정
-        String requestJson = null;
-        try {
-            String format = objectMapper.writeValueAsString(new TextToSpeechRequest(script));
-            requestJson = String.format(format);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+    private SoundFile getSoundFile(String voiceKey, Script script) {
+        byte[] bytes = sendRequest(
+                voiceKey,
+                new TextToSpeechRequest(script.message())
+        );
 
-        // HttpEntity 생성
-        HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
-
-        // 요청 전송 및 응답 받기
-        ResponseEntity<byte[]> response = restTemplate.exchange(url, HttpMethod.POST, entity, byte[].class);
-
-        // 상태 코드 확인 후 바이트 배열로 반환
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return new SoundFile(String.format("%s-%s.mp3", voiceKey, code), response.getBody());
-        } else {
-            throw new RuntimeException("Failed to retrieve audio data: " + response.getStatusCode());
-        }
+        return new SoundFile(
+                String.format("%s-%s.mp3", voiceKey, script.code()),
+                bytes
+        );
     }
 }
--- Backend/voice-service/src/main/java/com/ddasoom/voice_service/voice/adapter/out/elevenlabs/response/TextToSpeechResponse.java
+++
@@ -1,7 +0,0 @@
-package com.ddasoom.voice_service.voice.adapter.out.elevenlabs.response;
-
-public record TextToSpeechResponse(
-        byte[] bytes
-) {
-
-}
