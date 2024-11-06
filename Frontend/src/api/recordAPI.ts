1fb0eb554f5394dc39270d5ba73cf0e8c7f67c01
minyeong981 <rlaalsdud41@naver.com>
2024-11-06 11:24:04 +0900

    [FEAT] #80 api baseResponse 적용

--- Frontend/src/api/recordAPI.ts
+++ Frontend/src/api/recordAPI.ts
@@ -1,32 +1,34 @@
+import { BaseResponse } from '@/types/http/baseResponse';
 import { DiaryRequestBody, TrainingRequestBody } from '@/types/http/request';
+import { DailyData, MonthlyData } from '@/types/http/response';
 
 import axiosInstance from './axiosInstance';
 
 // 캘린더 월별 조회
-export const getMonthlyData = async (year: string, month: string) => {
-  const response = await axiosInstance.get('/diary/calendars', {
+export const getMonthlyData = async (year: string, month: string): Promise<BaseResponse<MonthlyData>> => {
+  const response = await axiosInstance.get<BaseResponse<MonthlyData>>('/diary/calendars', {
     params: { year, month },
   });
   return response.data;
 };
 
 // 캘린더 일별 조회(기록 상세)
-export const getDailyData = async (year: string, month: string, day: string) => {
-  const response = await axiosInstance.get('/diary/calendars', {
+export const getDailyData = async (year: string, month: string, day: string): Promise<BaseResponse<DailyData>> => {
+  const response = await axiosInstance.get<BaseResponse<DailyData>>('/diary/calendars', {
     params: { year, month, day },
   });
-  return response.data.data;
+  return response.data;
 };
 
 // 일상 기록 저장
-export const postDailyData = async (data: DiaryRequestBody) => {
-  const response = await axiosInstance.post('/diary/daily', data);
+export const postDailyData = async (data: DiaryRequestBody): Promise<BaseResponse<null>> => {
+  const response = await axiosInstance.post<BaseResponse<null>>('/diary/daily', data);
   return response.data;
 };
 
 // 훈련 종료(기록 저장)
-export const postTrainingData = async (data: TrainingRequestBody) => {
-  const response = await axiosInstance.post('/diary/training', data);
+export const postTrainingData = async (data: TrainingRequestBody): Promise<BaseResponse<null>> => {
+  const response = await axiosInstance.post<BaseResponse<null>>('/diary/training', data);
   console.log('훈련 기록 저장 성공');
   return response.data;
 };
