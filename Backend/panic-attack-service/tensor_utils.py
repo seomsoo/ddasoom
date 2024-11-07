2733a637fd8d6f6b0bab0a2c7257de9d7d9a38b6
김두열 <enduf768640@gmail.com>
2024-11-07 09:52:16 +0900

    [FEAT] #91 FastAPI 서버(Panic-Attack-Service) Spring Cloud Eureka Server 등록

--- Backend/panic-attack-service/tensor_utils.py
+++ Backend/panic-attack-service/tensor_utils.py
@@ -1,11 +1,13 @@
-import torch
-import numpy as np
 import pickle
 
+import numpy as np
+import torch
+
+
 def map_to_tensor(data):
-  with open("scaler.pkl", "rb") as f:
-    scaler = pickle.load(f)
+    with open("scaler.pkl", "rb") as f:
+        scaler = pickle.load(f)
 
-  scaled_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    scaled_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
 
-  return torch.tensor(scaled_data).float()
+    return torch.tensor(scaled_data).float()
