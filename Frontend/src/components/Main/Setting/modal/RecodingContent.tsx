2c55c10fda2e735a9f7489baafa63b3fd3125ccc
서민수 <minsu9812@naver.com>
2024-11-07 17:33:40 +0900

    feat/#92 설정페이지 추가

---
+++ Frontend/src/components/Main/Setting/modal/RecodingContent.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+export default function RecodingContent() {
+  const router = useRouter();
+
+  const goToRecodingPage = () => {
+    router.push('/main/setting/recoding');
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="text-2xl font-semibold mb-4">목소리 설정</h2>
+      <main>
+        <span>목소리 목록</span>
+      </main>
+      <section className="flex text-nowrap">
+        <button onClick={goToRecodingPage} className="bg-main4 p-4 px-4 rounded-full w-full">
+          <span>직접 녹음하기</span>
+        </button>
+        <button onClick={goToRecodingPage} className="bg-main4 p-4 px-4 rounded-full w-full">
+          <span>목소리 받아오기</span>
+        </button>
+      </section>
+    </div>
+  );
+}
