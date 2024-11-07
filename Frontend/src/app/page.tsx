09f575d93e3412c37f8f22897ba0a7603307a1dc
서민수 <minsu9812@naver.com>
2024-11-07 17:31:58 +0900

    feat/#92 메인 홈 리다이렉트 변경

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,17 +1,5 @@
-import React from 'react';
+import { redirect } from 'next/navigation';
 
-import Header from '@/components/Header';
-import Navbar from '@/components/Navbar';
-const Home = () => {
-  console.log('hi');
-  console.log('hi');
-
-  return (
-    <div>
-      <Header label="테스트" />
-      <Navbar />
-    </div>
-  );
-};
-
-export default Home;
+export default function Home() {
+  redirect('/main');
+}
