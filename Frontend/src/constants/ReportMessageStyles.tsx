a4819bf0e9dfe30c9317683702e243a1ac5bd792
서민수 <minsu9812@naver.com>
2024-11-08 16:37:51 +0900

    feat/#30 호흡하기 페이지 ui 수정

--- Frontend/src/constants/ReportMessageStyles.tsx
+++ Frontend/src/constants/ReportMessageStyles.tsx
@@ -1,18 +1,11 @@
 import Ddasom2 from '@/svgs/greensunglass.svg';
 
 export const reportMessageStyles = (count: number) => {
-  const countColor = count > 7 ? 'text-green-500'
-    : count > 5 ? 'text-blue-500'
-    : count > 3 ? 'text-yellow-500'
-    : 'text-gray-500';
-  
-  const messageColor = count > 7
-    ? 'text-green-800'
-    : count > 5
-    ? 'text-blue-800'
-    : count > 3
-    ? 'text-yellow-800'
-    : 'text-gray-500';
+  const countColor =
+    count > 7 ? 'text-green-500' : count > 5 ? 'text-blue-500' : count > 3 ? 'text-yellow-500' : 'text-gray-500';
+
+  const messageColor =
+    count > 7 ? 'text-green-800' : count > 5 ? 'text-blue-800' : count > 3 ? 'text-yellow-800' : 'text-gray-500';
 
   const message = (
     <>
@@ -22,18 +15,14 @@ export const reportMessageStyles = (count: number) => {
           <span className={countColor}>{count}일</span> 훈련을 성공적으로 이어가고 있어요!
         </>
       ) : (
-        <p>공황 발작 대처를 위해 꾸준한 훈련이 중요합니다. <br/> 앞으로 함께 연습해볼까요?</p>
+        <p>
+          공황 발작 대처를 위해 꾸준한 훈련이 중요합니다. <br /> 앞으로 함께 연습해볼까요?
+        </p>
       )}
     </>
   );
 
-  const icon = count > 7
-    ? <Ddasom2 />
-    : count > 5
-    ? <Ddasom2 />
-    : count > 3
-    ? <Ddasom2 />
-    : <Ddasom2 />;
+  const icon = count > 7 ? <Ddasom2 /> : count > 5 ? <Ddasom2 /> : count > 3 ? <Ddasom2 /> : <Ddasom2 />;
 
   return { message, messageColor, icon };
 };
