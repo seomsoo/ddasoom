26bcaf8fbbd45d1be42fd41c78e0194bba84478d
minyeong981 <rlaalsdud41@naver.com>
2024-11-09 20:03:40 +0900

    [FEAT] #113 비상연락처 api 설정

---
+++ Frontend/src/api/emergencyAPI.ts
@@ -0,0 +1,23 @@
+import { BaseResponse } from '@/types/http/baseResponse';
+import { SavePhoneRequestBody, SendMessageRequestBody } from '@/types/http/request';
+import { PhoneListData } from '@/types/http/response';
+
+import axiosInstance from './axiosInstance';
+
+// 비상 연락처 저장
+export const postSavePhoneData = async (data: SavePhoneRequestBody): Promise<BaseResponse<null>> => {
+  const response = await axiosInstance.post<BaseResponse<null>>('/emergency/phone-book', data);
+  return response.data;
+};
+
+// 비상 연락처로 문자
+export const postSendMessageData = async (data: SendMessageRequestBody): Promise<BaseResponse<null>> => {
+  const response = await axiosInstance.post<BaseResponse<null>>('/emergency/send-message', data);
+  return response.data;
+};
+
+// 비상 연락처 목록 조회
+export const getPhoneData = async (): Promise<BaseResponse<PhoneListData>> => {
+  const response = await axiosInstance.get<BaseResponse<PhoneListData>>('/emergency/phone-books');
+  return response.data;
+};
