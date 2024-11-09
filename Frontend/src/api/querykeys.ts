f8d8cf91932915fc88a1e311a128d3155a7c2ec9
minyeong981 <rlaalsdud41@naver.com>
2024-11-09 20:03:40 +0900

    [FEAT] #113 비상연락처 api 설정

--- Frontend/src/api/querykeys.ts
+++ Frontend/src/api/querykeys.ts
@@ -2,6 +2,7 @@ const queryKeys = {
   MONTHLY_RECORD: 'monthlyRecord',
   DAILY_RECORD: 'dailyRecord',
   REPORT: 'report',
+  EMERGENCY_PHONE_NUMBER: 'emergencyPhoneNumber',
 };
 
 export default queryKeys;
