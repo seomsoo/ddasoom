9024b648f42818e28be108617f6c22988838b557
손다인 <dain0826@naver.com>
2024-11-10 18:38:30 +0900

    [FEAT] #107 상황종료 activity 추가

---
+++ wear/app/src/main/java/com/ddasoom/wear/activity/EndActivity.kt
@@ -0,0 +1,24 @@
+package com.ddasoom.wear.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.ddasoom.wear.R
+
+class EndActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end)
+
+        // "확인" 버튼 클릭 이벤트
+        val confirmButton = findViewById<Button>(R.id.btn_confirm_breath)
+        confirmButton.setOnClickListener {
+            // 메인 화면으로 이동
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish() // 현재 액티비티 종료
+        }
+    }
+}
