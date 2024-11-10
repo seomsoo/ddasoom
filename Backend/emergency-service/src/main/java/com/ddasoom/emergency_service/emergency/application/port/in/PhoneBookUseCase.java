6309af1a988375d88332698a524459fbab55dd17
ShinBeomKim <shinbeomkim11@gmail.com>
2024-11-10 18:58:50 +0900

    [REFACTOR] #120 긴급 연락처 삭제 반환 상태코드 204로 변경

--- Backend/emergency-service/src/main/java/com/ddasoom/emergency_service/emergency/application/port/in/PhoneBookUseCase.java
+++ Backend/emergency-service/src/main/java/com/ddasoom/emergency_service/emergency/application/port/in/PhoneBookUseCase.java
@@ -11,5 +11,5 @@ public interface PhoneBookUseCase {
 
     void sendMessage(Long userId, String username);
 
-    List<PhoneBookResponse> deletePhoneBook(Long phoneBookId, Long userId);
+    void deletePhoneBook(Long phoneBookId);
 }
