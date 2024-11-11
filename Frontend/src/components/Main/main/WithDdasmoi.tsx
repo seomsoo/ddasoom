0bf15d758598f41e2dd083c876f6ba67bdb03a9c
minyeong981 <rlaalsdud41@naver.com>
2024-11-12 03:08:52 +0900

    [FEAT] #138 메인화면 조회 api 연결

---
+++ Frontend/src/components/Main/main/WithDdasmoi.tsx
@@ -0,0 +1,93 @@
+'use client';
+import Link from 'next/link';
+
+import Navbar from '@/components/Common/Navbar';
+import Character from '@/components/Main/main/Character';
+import Interaction from '@/components/Main/main/interaction';
+import LevelBar from '@/components/Main/main/LevelBar';
+import Watch from '@/components/Main/main/Watch';
+import Window from '@/components/Main/main/Window';
+import EmergencyContent from '@/components/Main/Setting/modal/EmergencyContent';
+import EmergencyModal from '@/components/Main/Setting/modal/EmergencyModal';
+import HelpContent from '@/components/Main/Setting/modal/HelpContent';
+import HelpModal from '@/components/Main/Setting/modal/HelpModal';
+import MissionContent from '@/components/Main/Setting/modal/MissionContent';
+import MissionModal from '@/components/Main/Setting/modal/MissionModal';
+import Setting from '@/svgs/setting.svg';
+import Bed from '@/svgs/bed.svg';
+import SoundOn from '@/svgs/soundOn.svg';
+import Bookcase from '@/svgs/bookcase.svg';
+import { useSelector } from 'react-redux';
+import { getCharacterData } from '@/api/mainAPI';
+import { RootState } from '@/store';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import queryKeys from '@/api/querykeys';
+import { useEffect } from 'react';
+
+export default function WithDdasomi() {
+  const userId = useSelector((state: RootState) => state.auth.userId);
+  const queryClient = useQueryClient();
+  const { data: characterData } = useQuery({
+    queryKey: [queryKeys.CHARACTER, userId],
+    queryFn: () => getCharacterData(Number(userId)),
+  });
+
+  const ddasomiData = characterData?.data;
+
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: [queryKeys.CHARACTER, userId] });
+  });
+
+  return (
+    <div className="bg-[#C7C0B3] h-screen -m-4">
+      <header className="flex  flex-col w-full h-72 bg-main4 p-6 border-b-8">
+        <article className="flex justify-between   w-full ">
+          <LevelBar
+            level={ddasomiData?.level ?? 0}
+            experience={ddasomiData?.experience ?? 0}
+            experiencePercent={ddasomiData?.experiencePercent ?? 0}
+          />
+          <EmergencyModal ContentComponent={EmergencyContent} />
+        </article>
+
+        <div className="flex justify-between ">
+          <div className="mt-8 ml-7">
+            <Window />
+          </div>
+          <div className="mt-8 mr-14">
+            <Watch />
+          </div>
+          <div className="absolute gap-6 right-6 top-40 z-10 flex flex-col">
+            <HelpModal ContentComponent={HelpContent} />
+            <Link href="/main/setting">
+              <Setting />
+            </Link>
+            <SoundOn />
+            <MissionModal ContentComponent={MissionContent} />
+          </div>
+        </div>
+      </header>
+
+      <main className="flex flex-col bg-[#D8D1C3] items-center h-52">
+        <div className="absolute left-7 top-56 ">
+          <Bed />
+        </div>
+        <div className="absolute right-16 z-0 top-52">
+          <Bookcase />
+        </div>
+        <div>
+          <Character />
+        </div>
+      </main>
+      <section className="flex flex-col p-3">
+        <Interaction
+          continuousTrainingDays={ddasomiData?.continuousTrainingDays ?? 0}
+          strokeCount={ddasomiData?.strokeCount ?? 0}
+          hugCount={ddasomiData?.hugCount ?? 0}
+          playCount={ddasomiData?.playCount ?? 0}
+        />
+      </section>
+      <Navbar />
+    </div>
+  );
+}
