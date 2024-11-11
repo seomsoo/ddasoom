67990b363eefa4f77fbdc6c7ddbaae536becf6b7
minyeong981 <rlaalsdud41@naver.com>
2024-11-11 11:04:39 +0900

    [FEAT] #126 웹->앱 현재 위치 요청

---
+++ Frontend/src/app/record/selfDiagnosis/hospitals/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import NearbyHospitalsMap from '@/components/Record/SelfDiagnosis/NearbyHospitalsMap';
+
+export default function HospitalsPage() {
+  return (
+    <div>
+      <h1>근처 병원</h1>
+      <NearbyHospitalsMap />
+    </div>
+  );
+}
