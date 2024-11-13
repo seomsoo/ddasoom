8e0fe0758af35e5b24199b9de6a735d4cee6bba3
monam2 <kangcw0107@gmail.com>
2024-11-13 17:53:15 +0900

    [FEAT] #157 앱 배포용 수정

---
+++ WebView/app/(app)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+
+const AppLayout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false }} initialRouteName="(login)">
+      <Stack.Screen name="(login)" />
+      <Stack.Screen name="authorized" />
+      <Stack.Screen name="ble" />
+      <Stack.Screen name="breath" />
+    </Stack>
+  );
+};
+
+export default AppLayout;
