3e02223ed28e43a0111455cf950af146c7c26c8b
minyeong981 <rlaalsdud41@naver.com>
2024-11-13 17:37:12 +0900

    [DESIGN] #156 메인 따솜이 크기 변경

--- Frontend/src/components/Main/main/Character.tsx
+++ Frontend/src/components/Main/main/Character.tsx
@@ -14,15 +14,18 @@ export default function Character({ level, currentInteractionGif }: LevelProps) 
   const getCharacterSvg = () => {
     // 상호작용 GIF가 있을 경우 우선 표시
     if (currentInteractionGif) {
-      return <img src={currentInteractionGif} className="w-24 h-28" alt="Interaction GIF" />;
+      if (level === 2) {
+        return <img src={currentInteractionGif} className="w-32 h-36" alt="Interaction GIF" />;
+      }
+      return <img src={currentInteractionGif} className="w-28 h-32" alt="Interaction GIF" />;
     }
 
     // 기본 캐릭터 표시
     if (level === 1) {
-      return <img src={Babysomi.src} className="w-24 h-28" alt="Level 1 Character" />;
+      return <img src={Babysomi.src} className="w-28 h-32" alt="Level 1 Character" />;
     }
     if (level === 2) {
-      return <img src={Lv2.src} className="w-24 h-28" alt="Level 2 Character" />;
+      return <img src={Lv2.src} className="w-32 h-36" alt="Level 2 Character" />;
     }
     if (level === 3) {
       return <Level3 />;
@@ -38,7 +41,7 @@ export default function Character({ level, currentInteractionGif }: LevelProps) 
   };
 
   return (
-    <div className="flex flex-col items-center mt-10">
+    <div className="flex flex-col items-center mt-8">
       <div className="absolute bg-gray1 p-4 rounded-2xl shadow-md text-center text-sm max-w-72 top-64">
         <span>{getBalloonText()}</span>
         <div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 translate-y-full w-4 h-4 bg-gray1 rotate-45 " />
