392e47b3bf88ee3c8b47b79faddbfdf2bde94c28
박지훈 <pjh2996@naver.com>
2024-11-13 11:54:15 +0900

    [FEAT] #142 워치 모델 적용

--- wear/app/src/main/java/com/ddasoom/wear/service/ForegroundService.kt
+++ wear/app/src/main/java/com/ddasoom/wear/service/ForegroundService.kt
@@ -18,6 +18,7 @@ import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.ddasoom.wear.R
 import com.ddasoom.wear.activity.MainActivity
+import com.ddasoom.wear.activity.RequestActivity
 import com.ddasoom.wear.constants.Constants
 import com.google.android.gms.wearable.MessageClient
 import com.google.android.gms.wearable.Wearable
@@ -29,6 +30,9 @@ class ForegroundService : Service(), HeartRateManager.HeartRateListener {
   private lateinit var messageClient: MessageClient
   private var nodeId: String? = null
   private lateinit var wakeLock: PowerManager.WakeLock
+
+  private val bpmData = ArrayList<Float>() // 1분 단위로 BPM 데이터를 저장
+  private val hrvCalculator = HrvCalculator()
 
   companion object {
     var heartRateCallback: ((Int) -> Unit)? = null // 심박수 데이터를 전달하는 콜백
@@ -123,6 +127,25 @@ class ForegroundService : Service(), HeartRateManager.HeartRateListener {
     // 심박수 데이터 콜백 호출
     heartRateCallback?.invoke(heartRate)
 
+    // 유효한 심박수만 처리 (0 이상)
+    if (heartRate > 0) {
+      bpmData.add(heartRate.toFloat())
+
+      // 유효 심박수가 충분히 수집되면 1분 단위로 HRV 지표 계산
+      if (bpmData.size >= 60) {
+        val hrvMetrics = hrvCalculator.calculateHrvMetrics(bpmData)
+        Log.d(Constants.TAG, "HRV Metrics: ${hrvMetrics.joinToString(", ")}")
+
+        // 필요한 작업 수행 (예: 모델 입력 준비)
+        processHrvMetrics(hrvMetrics)
+
+        // 데이터 초기화
+        bpmData.clear()
+      }
+    } else {
+      // 심박수가 유효하지 않은 경우 로그 출력
+      Log.d(Constants.TAG, "Invalid heart rate received: $heartRate (ignored)")
+    }
   }
 
   // 심박수 데이터를 전송하는 메서드
@@ -171,6 +194,29 @@ class ForegroundService : Service(), HeartRateManager.HeartRateListener {
     // 서비스 종료 시 콜백 해제
     heartRateCallback = null
   }
+
+  private fun processHrvMetrics(hrvMetrics: FloatArray) {
+    // PyTorch 모델 실행
+    val result = PytorchModelUtils.runModel(this, hrvMetrics)
+    Log.d(Constants.TAG, "PyTorch 모델 결과: ${result.joinToString(", ")}")
+
+    // 모델 출력 값이 0.5 이상인지 확인
+    if (result.isNotEmpty() && result[0] >= 0.5f) {
+      Log.d(Constants.TAG, "모델 결과가 0.5 이상입니다. RequestActivity를 실행합니다.")
+      startRequestActivity()
+    } else {
+      Log.d(Constants.TAG, "모델 결과가 0.5 미만입니다. 아무 작업도 수행하지 않습니다.")
+    }
+  }
+
+  private fun startRequestActivity() {
+    val intent = Intent(this, RequestActivity::class.java).apply {
+      flags = Intent.FLAG_ACTIVITY_NEW_TASK // 서비스에서 액티비티를 실행하려면 이 플래그 필요
+    }
+    startActivity(intent)
+    Log.d(Constants.TAG, "RequestActivity를 실행했습니다.")
+  }
+
 
 
   override fun onBind(intent: Intent?): IBinder? = null
