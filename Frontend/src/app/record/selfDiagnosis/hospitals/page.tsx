c9d8749850ed15ea727817ac7a550aa9d90549cb
서민수 <minsu9812@naver.com>
2024-11-14 15:05:04 +0900

    병원 ui

--- Frontend/src/app/record/selfDiagnosis/hospitals/page.tsx
+++ Frontend/src/app/record/selfDiagnosis/hospitals/page.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-
 import NearbyHospitalsMap from '@/components/Record/SelfDiagnosis/NearbyHospitalsMap';
 
 export default function HospitalsPage() {
   return (
-    <div>
-      <h1>근처 병원</h1>
-      <NearbyHospitalsMap />
+    <div className="-m-4 h-screen">
+      <header className="fixed top-0 z-10 flex flex-col w-full items-center">
+        <div className="bg-[#d2e4bd] rounded-b-3xl text-center h-24 justify-center flex flex-col w-full">
+          <span className="text-2xl  font-nanumBold text-main4">근처 병원</span>
+          <span className="opacity-45 mt-1">내 위치에서 가장 가까운 순으로 보여요.</span>
+        </div>
+      </header>
+      <main className=" w-full">
+        <NearbyHospitalsMap />
+      </main>
     </div>
   );
 }
