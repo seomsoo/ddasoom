f6bd9b915eaad46d35d8a69360b6b1f08f387587
minyeong981 <rlaalsdud41@naver.com>
2024-11-14 21:37:11 +0900

    [TEST] #181 메인 withDdasomi.tsx 변경

--- Frontend/src/components/Common/ClientComponent.tsx
+++ Frontend/src/components/Common/ClientComponent.tsx
@@ -3,8 +3,7 @@
 import useAuth from '@/hooks/useGetToken';
 
 export default function ClientComponent({ children }: { children: React.ReactNode }) {
-  const { token } = useAuth();
-  console.log('토큰 :', token);
+  useAuth();
 
   return <>{children}</>;
 }
