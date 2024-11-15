d61fdca6957695d3eda30ac39fe035df4dcf168e
monam2 <kangcw0107@gmail.com>
2024-11-15 10:23:41 +0900

    [FEAT] #177 [앱] 로그인 시 토큰 리페치

--- WebView/app/_layout.tsx
+++ WebView/app/_layout.tsx
@@ -9,20 +9,33 @@ import useNotificationStore from "@/zustand/notificationStore";
 import { useGlobalFonts } from "@/hooks/useGlobalFonts"; // useGlobalFonts 파일의 경로에 맞게 설정
 import useAuthStore from "@/zustand/authStore";
 import useSendToken from "@/hooks/useSendToken";
+import { signIn } from "@/services/auth";
 
 const Root = () => {
-  const { token } = useAuthStore();
+  const { token, setToken, userEmail } = useAuthStore();
   const fontsLoaded = useGlobalFonts();
   useNotification(); // Initialize notification setup on app load
   const { expoPushToken, notification } = useNotificationStore();
 
   const statusBarHeight = Platform.OS === "android" ? StatusBar.currentHeight : 0;
 
+  const refetchToken = async () => {
+    try {
+      const { token } = await signIn(userEmail);
+      setToken(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
-    getKeyHashAndroid().then(console.log);
+    // getKeyHashAndroid().then(console.log);
     initializeKakaoSDK(`${process.env.EXPO_PUBLIC_KAKAO_NATIVE_KEY}`);
     console.log("엑스포 푸시 토큰 : ", expoPushToken);
+
     if (token && token !== "") {
+      // 토큰 리프레시
+      refetchToken();
       router.push("(app)/authorized");
     }
   }, [expoPushToken, token]);
