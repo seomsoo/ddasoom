b5fed49da4d9840f4024d26bd88f595ecc7446b5
서민수 <minsu9812@naver.com>
2024-11-16 16:29:24 +0900

    feat/#200 자가진단 수정

--- Frontend/src/app/record/selfDiagnosis/check/page.tsx
+++ Frontend/src/app/record/selfDiagnosis/check/page.tsx
@@ -1,11 +1,9 @@
-import Header from '@/components/Common/Header';
 import CheckList from '@/components/Record/SelfDiagnosis/CheckList';
 
 export default function SelfDiagnosisDetailPage() {
   return (
-    <div>
-      <Header label="자가진단" />
+    <>
       <CheckList />
-    </div>
+    </>
   );
 }
--- Frontend/src/components/Record/SelfDiagnosis/QuestionText.tsx
+++ Frontend/src/components/Record/SelfDiagnosis/QuestionText.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import Image from 'next/image';
 
-interface QuestionTextProps {
-  text: string;
+interface QuestionProps {
+  question: string;
+  gif: string;
 }
 
-export default function QuestionText({ text }: QuestionTextProps) {
+export default function Question({ question, gif }: QuestionProps) {
+  const questionLines = question.split(',').map((line, index) => (
+    <span key={index}>
+      {line.trim()}
+      <br />
+    </span>
+  ));
+
   return (
-    <h2
-      className="text-2xl font-nanumBold mt-10 w-72 h-36 "
-      dangerouslySetInnerHTML={{
-        __html: text.replace(/(?:\n|,)/g, '<br />'),
-      }}
-    />
+    <div className="mt-5 flex flex-col items-center">
+      <span className="font-hakgyoansimR h-24 text-3xl">{questionLines}</span>
+      <div className="flex flex-col items-center w-full">
+        <div className="w-72 h-72 items-center border-[8px] mt-4 border-[#e4f5db] bg-white rounded-full relative flex justify-center overflow-hidden">
+          <Image src={gif} alt="Self Diagnosis Visual" width={192} height={192} className="w-48 h-48" />
+        </div>
+      </div>
+    </div>
   );
 }
