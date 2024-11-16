b3f2cdcfeb127bb9f446f5883466372e95846419
monam2 <kangcw0107@gmail.com>
2024-11-17 06:15:47 +0900

    [FEAT] #191 음성 재생을 위한 데이터 추가

--- WebView/constant/BreathData.ts
+++ WebView/constant/BreathData.ts
@@ -1,29 +1,27 @@
-import { BreathData } from "@/types/ddasoom";
-
 const breathData: Record<"shortTime" | "basicTime" | "longTime", BreathData> = {
   shortTime: {
     type: "shortTime",
     stages: [
-      { duration: 4000, description: "들이마시기" },
-      { duration: 4000, description: "참기" },
-      { duration: 4000, description: "내쉬기" },
-      { duration: 4000, description: "참기" },
+      { fileId: 1, duration: 4000, description: "들이마시기" },
+      { fileId: 2, duration: 4000, description: "참기" },
+      { fileId: 3, duration: 4000, description: "내쉬기" },
+      { fileId: 2, duration: 4000, description: "참기" },
     ],
   },
   basicTime: {
     type: "basicTime",
     stages: [
-      { duration: 4000, description: "들이마시기" },
-      { duration: 7000, description: "참기" },
-      { duration: 8000, description: "내쉬기" },
+      { fileId: 1, duration: 4000, description: "들이마시기" },
+      { fileId: 2, duration: 7000, description: "참기" },
+      { fileId: 3, duration: 8000, description: "내쉬기" },
     ],
   },
   longTime: {
     type: "longTime",
     stages: [
-      { duration: 5000, description: "들이마시기" },
-      { duration: 7000, description: "내쉬기" },
-      { duration: 3000, description: "참기" },
+      { fileId: 1, duration: 5000, description: "들이마시기" },
+      { fileId: 3, duration: 7000, description: "내쉬기" },
+      { fileId: 2, duration: 3000, description: "참기" },
     ],
   },
 };
