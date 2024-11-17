1739e44fb0c21944e09f7e0098e355e300d96fd1
서민수 <minsu9812@naver.com>
2024-11-17 17:00:35 +0900

    fix/#206 로딩  모달 수정

--- Frontend/src/components/Main/Setting/voice/LoadingModal.tsx
+++ Frontend/src/components/Main/Setting/voice/LoadingModal.tsx
@@ -2,15 +2,19 @@
 
 import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
-import Cancel from '@/svgs/cancel.svg';
 import Loading from '@/videos/loading.gif';
 
 export default function LoadingModal() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true); // 모달이 열려 있는 상태로 초기화
+  const router = useRouter();
 
-  const closeModal = () => setIsOpen(false);
+  const closeModal = () => {
+    setIsOpen(false);
+    router.push('/main/setting'); // 버튼 클릭 시 해당 경로로 이동
+  };
 
   return (
     <>
@@ -28,16 +32,17 @@ export default function LoadingModal() {
               animate={{ scale: 1 }}
               exit={{ scale: 0.9 }}
               transition={{ duration: 0.3 }}>
-              <button onClick={closeModal} className="absolute z-10 top-4 right-5">
-                <Cancel />
-              </button>
               <div className="max-h-[60vh] min-h-[30vh]">
-                <div className=" relative flex flex-col items-center">
-                  <span className="text-xl font-hakgyoansimR text-center mt-16">
+                <div className="relative flex flex-col items-center">
+                  <span className="text-xl font-hakgyoansimR text-center mt-5">
                     따뜻한 목소리를 담아내고 있습니다. <br />
-                    잠시만 기다려 주세요.
+                    설정으로 돌아가 잠시만 기다려 주세요.
                   </span>
+                  <span className="text-xs mt-1 text-gray4 font-nanumBold">(약 40초 소요)</span>
                   <Image src={Loading} className="w-24 h-24 " alt="Loading" />
+                  <button onClick={closeModal} className="px-20 rounded-full py-2 bg-button1 text-white font-nanumBold">
+                    <span>확인</span>
+                  </button>
                 </div>
               </div>
             </motion.div>
