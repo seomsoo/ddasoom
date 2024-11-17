e263253a9cb8c0269f45c0c1398b62881cb120c8
minyeong981 <rlaalsdud41@naver.com>
2024-11-17 20:44:02 +0900

    [FEAT] #210 아두이노 연결버튼 수정

--- Frontend/src/components/Main/Setting/ArduinoSetting.tsx
+++ Frontend/src/components/Main/Setting/ArduinoSetting.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-
+import Enter from '@/svgs/enterIcon.svg';
 const ArduinoSetting = () => {
   // 메시지 보내기
   const sendArduinoRequest = () => {
@@ -14,8 +14,11 @@ const ArduinoSetting = () => {
   };
 
   return (
-    <button className=" bg-main4 p-5 rounded-full text-left" onClick={sendArduinoRequest}>
+    <button
+      className=" bg-main4 p-5 rounded-full flex justify-between text-left font-nanumBold w-full"
+      onClick={sendArduinoRequest}>
       <span>따솜키링 연결</span>
+      <Enter />
     </button>
   );
 };
