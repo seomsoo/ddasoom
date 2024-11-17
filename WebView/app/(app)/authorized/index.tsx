d336d2e43476406fb7f5e8c290725fba9b2b97fb
monam2 <kangcw0107@gmail.com>
2024-11-18 00:57:27 +0900

    [FEAT] #208 웹뷰 메인 페이지 메세지 핸들러 분리

--- WebView/app/(app)/authorized/index.tsx
+++ WebView/app/(app)/authorized/index.tsx
@@ -2,31 +2,24 @@ import useSendToken from "@/hooks/useSendToken";
 import useVoiceRecord from "@/hooks/useVoiceRecord";
 import { postPanicAtFirst } from "@/services/panic";
 import { deletePanicInfoFromStorage, loadPanicInfoFromStorage } from "@/storage/panic";
-import { vibrate, vibrateOff } from "@/utils/vibrate";
 import useAuthStore from "@/zustand/authStore";
-import { logout } from "@react-native-kakao/user";
-import { router } from "expo-router";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { BackHandler, Platform, StatusBar } from "react-native";
-import WebView, { WebViewMessageEvent } from "react-native-webview";
+import WebView from "react-native-webview";
 import type { WebView as WebViewType } from "react-native-webview";
 import PanicDataModal from "@/components/authorized/PanicDataModal"; // 모달 컴포넌트 가져오기
-import {
-  checkPushNotificationPermission,
-  promptDisablePushNotification,
-  requestPushNotificationPermission,
-} from "@/utils/permissions";
+import { checkPushNotificationPermission } from "@/utils/permissions";
 import useSendLocation from "@/hooks/useSendLocation";
-import { loadBreathTypeFromStorage, saveBreathTypeToStorage } from "@/storage/breath";
-import { sendMessageToWeb } from "@/utils/sendMessageToWeb";
+import { loadBreathTypeFromStorage } from "@/storage/breath";
 import useVoiceKeyStore from "@/zustand/voiceKeyStore";
 import usePhoneStore from "@/zustand/contactStore";
 import BleModal from "@/components/authorized/BleModal";
+import { createMessageHandler } from "@/utils/messageHandler";
 
 const AuthedScreen = () => {
   const webViewRef = useRef<WebViewType | null>(null);
-  const { recordUri, startRecording, stopRecording, sendRecording } = useVoiceRecord();
-  const { token, userName, userId, userEmail } = useAuthStore();
+  const { startRecording, stopRecording, sendRecording } = useVoiceRecord();
+  const { token, userName, userEmail } = useAuthStore();
   const { voiceKey, setVoiceKey } = useVoiceKeyStore();
   const [modalVisible, setModalVisible] = useState(false);
   const [bleModalVisible, setBleModalVisible] = useState(false);
@@ -39,102 +32,6 @@ const AuthedScreen = () => {
   const statusBarHeight = Platform.OS === "android" ? StatusBar.currentHeight : 0;
   const sendTokenToWeb = useSendToken(webViewRef);
 
-  // 웹에서 메시지 받기
-  const handleMessage = async (event: WebViewMessageEvent) => {
-    const data = event.nativeEvent.data;
-    const { title, content }: WebMessageDto = JSON.parse(data);
-    switch (title) {
-      case "GETTOKEN":
-        console.log("웹에서 토큰 요청함");
-        sendTokenToWeb();
-        return;
-      case "LOGOUT":
-        await logout();
-        console.log("로그아웃 됨");
-        BackHandler.removeEventListener("hardwareBackPress", backPress);
-        router.push("(app)/(login)");
-        return;
-      case "RECORD":
-        // content가 RecordMessage 타입인지 확인
-        if (typeof content === "object" && "state" in content) {
-          const recordContent = content as RecordMessgae;
-          if (recordContent.state === "ONAIR" && recordContent.name) {
-            await startRecording();
-            return;
-          }
-          if (recordContent.state === "STOPAIR" && recordContent.name) {
-            await stopRecording();
-            return;
-          }
-          if (recordContent.state === "OFFAIR" && recordContent.name) {
-            const uri = await stopRecording();
-            if (uri) {
-              await sendRecording(uri, recordContent.name);
-            } else {
-              console.log("녹음 파일이 저장되지 않았습니다.");
-            }
-            return;
-          }
-        } else {
-          console.error("잘못된 RECORD 메시지 형식:", content);
-        }
-        return;
-
-      case "VIBRATE":
-        vibrate(content as string);
-        return;
-      case "VIBRATEOFF":
-        vibrateOff();
-        return;
-      case "NOTI":
-        checkPushPermission(); // 푸시 권한 여부 전송
-        if (content === "yes") {
-          requestPushNotificationPermission();
-        } else if (content === "no") {
-          promptDisablePushNotification();
-        }
-        return;
-      case "ARDSETTING":
-        setBleModalVisible(true);
-        return;
-      case "SOS":
-        BackHandler.removeEventListener("hardwareBackPress", backPress);
-        const storedBreathType = await loadBreathTypeFromStorage();
-        router.push(`(app)/breath?breathType=${storedBreathType ?? "basicTime"}`);
-        return;
-      case "GPS":
-        sendLocationToWebView();
-        return;
-      case "BREATH":
-        if (!content) {
-          // 설정된 목소리 요청
-          const storedBreathType = await loadBreathTypeFromStorage();
-          sendMessageToWeb({ webViewRef, title: "BREATH", content: storedBreathType });
-          return;
-        }
-        // 목소리 설정
-        saveBreathTypeToStorage(content as string);
-        return;
-      case "AIVOICE":
-        if (!content) {
-          console.log("웹에서 목소리 요청함. 보내줄 목소리는 ", voiceKey);
-          sendMessageToWeb({ webViewRef, title: "AIVOICE", content: voiceKey });
-          return;
-        }
-        setVoiceKey(content as string);
-        return;
-      case "PHONELIST":
-        sendMessageToWeb({ webViewRef, title: "PHONELIST", content: phoneNumbers });
-        return;
-      case "SETPHONE":
-        console.log("리스트", content);
-        setPhoneNumbers(content as any);
-        return;
-      default:
-        console.log(title, " : ", content);
-    }
-  };
-
   const backPress = useCallback(() => {
     if (webViewRef.current) {
       webViewRef.current.goBack();
@@ -142,6 +39,21 @@ const AuthedScreen = () => {
     }
     return false;
   }, []);
+
+  const handleMessage = createMessageHandler({
+    webViewRef,
+    sendTokenToWeb,
+    startRecording,
+    stopRecording,
+    sendRecording,
+    sendLocationToWebView,
+    setVoiceKey,
+    voiceKey,
+    setPhoneNumbers,
+    phoneNumbers,
+    setBleModalVisible,
+    backPress,
+  });
 
   const fetchPanicData = async () => {
     const data = await loadPanicInfoFromStorage();
@@ -188,10 +100,6 @@ const AuthedScreen = () => {
     await deletePanicInfoFromStorage();
   };
 
-  const handleBleCancel = () => {
-    setBleModalVisible(false);
-  };
-
   const checkPushPermission = async () => {
     const isPermission = await checkPushNotificationPermission();
     const data = JSON.stringify({ title: "ISPUSH", content: isPermission });
@@ -224,7 +132,7 @@ const AuthedScreen = () => {
   return (
     <>
       <StatusBar />
-      {token && (
+      {token && userName && userEmail && (
         <WebView
           ref={webViewRef}
           style={{ paddingTop: statusBarHeight, flex: 1 }}
