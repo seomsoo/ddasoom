120ee1451c5cb09deee34318db73c748675aaadd
박지훈 <pjh2996@naver.com>
2024-11-19 09:30:04 +0900

    [CHORE] dump 파일 업로드

---
+++ exec/dump/ddasomi_info.sql
@@ -0,0 +1,7 @@
+INSERT INTO ddasoom.ddasomi_info (experience, hug_count, play_count, stroke_count, ddasomi_info_id, user_id) VALUES (0, 0, 0, 0, 1, 1);
+INSERT INTO ddasoom.ddasomi_info (experience, hug_count, play_count, stroke_count, ddasomi_info_id, user_id) VALUES (0, 0, 0, 0, 2, 2);
+INSERT INTO ddasoom.ddasomi_info (experience, hug_count, play_count, stroke_count, ddasomi_info_id, user_id) VALUES (0, 0, 0, 0, 3, 3);
+INSERT INTO ddasoom.ddasomi_info (experience, hug_count, play_count, stroke_count, ddasomi_info_id, user_id) VALUES (0, 0, 0, 0, 4, 4);
+INSERT INTO ddasoom.ddasomi_info (experience, hug_count, play_count, stroke_count, ddasomi_info_id, user_id) VALUES (0, 0, 0, 0, 5, 5);
+INSERT INTO ddasoom.ddasomi_info (experience, hug_count, play_count, stroke_count, ddasomi_info_id, user_id) VALUES (95, 0, 0, 0, 6, 6);
+INSERT INTO ddasoom.ddasomi_info (experience, hug_count, play_count, stroke_count, ddasomi_info_id, user_id) VALUES (0, 0, 0, 0, 7, 7);
